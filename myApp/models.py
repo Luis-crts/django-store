103353d73e02240662fdc94ddf46dab310c06e2e
Luis-crts <luiscortes13531@gmail.com>
2025-11-28 18:36:15 -0300

    creacion models

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productos')
+    nombre = models.CharField(max_length=150)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    imagen = models.URLField(blank=True)  # opcional
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Carrito(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    creado = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Carrito de {self.usuario.username}"
+
+
+class CarritoItem(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE, related_name='items')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.cantidad} x {self.producto.nombre}"
+    
+class Orden(models.Model):
+    ESTADOS = (
+        ('solicitado', 'Solicitado'),
+        ('aprobado', 'Aprobado'),
+        ('en_proceso', 'En proceso'),
+        ('realizada', 'Realizada'),
+        ('entregada', 'Entregada'),
+        ('finalizada', 'Finalizada'),
+        ('cancelada', 'Cancelada'),
+    )
+
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    estado = models.CharField(max_length=20, choices=ESTADOS, default='solicitado')
+
+    def __str__(self):
+        return f"Orden #{self.id} - {self.usuario.username}"
+    
+class OrdenItem(models.Model):
+    orden = models.ForeignKey(Orden, on_delete=models.CASCADE, related_name='items')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.cantidad} x {self.producto.nombre}"
