d976e49ae02518da96b8668541d8d164c0ad91c7
Luis-crts <luiscortes13531@gmail.com>
2025-12-17 15:13:46 -0300

    se elimino un boton y 1 urls

--- djangoStore/urls.py
+++ djangoStore/urls.py
@@ -9,17 +9,22 @@ from myApp.views_reports import vista_reporte, reporte_dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # publicas
     path('', views.home, name='home'),
     path('producto/<int:pk>/', views.detalle_producto, name='detalle_producto'),
     path('producto/<int:pk>/solicitar/', views.solicitar_producto, name='solicitar_producto'),
     path('seguimiento/', views.buscar_pedido, name='buscar_pedido'),
-    path('seguimiento/<str:token>/', views.seguimiento_pedido, name='seguimiento_pedido'),
-    path('admin/seguimientos/', views.listar_seguimientos, name='listar_seguimientos'),
-    path('admin/reportes/', views.reportes, name='reportes'),
-    path('admin/reporte/', vista_reporte, name='vista_reporte'),
-    path('admin/dashboard/', reporte_dashboard, name='reporte_dashboard'),
+    path('pedido/<str:token>/', views.seguimiento_pedido, name='seguimiento_pedido'),
+
+    # Admin
+    path('seguimientos/', views.listar_seguimientos, name='listar_seguimientos'),
+    path('reportes/', vista_reporte, name='vista_reporte'),
+    path('dashboard/', reporte_dashboard, name='reporte_dashboard'),
+
     path('api/', include('myApp.api_urls')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
